6e050c3af6d671a654394e3f3800690d347bff78
Susie Lyu <lvsusie020@gmail.com>
2022-10-30 01:11:55 +0800

    “Update”

--- src/views.py
+++ src/views.py
@@ -136,7 +136,7 @@ def add_transfers():
         if session['user_available']:
             reader = AddTransferForm(request.form)
             if request.method == 'POST':
-                models.addTransfers({"pos_transfer_id": reader.pos_transfer_id.data, "date_key": reader.date_key.data, "book_key": reader.book_key.data, "clerk_id": reader.clerk_id.data, "shopper_id": reader.shopper_id.data, "promotion_key": reader.promotion_key.data, "store_key": reader.store_key.data})
+                models.addTransfers({"POS_transfer_id": reader.POS_transfer_id.data, "date_key": reader.date_key.data, "book_key": reader.book_key.data, "clerk_id": reader.clerk_id.data, "shopper_id": reader.shopper_id.data, "promotion_key": reader.promotion_key.data, "store_key": reader.store_key.data})
                 #return redirect(url_for('show_books'))
             return render_template('addTransfers.html', reader=reader)
     except Exception as e:
