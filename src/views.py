d69f7b4a46169d9cf9916ca1267d3f00f023cdd4
Susie Lyu <lvsusie020@gmail.com>
2022-11-01 04:58:50 +0800

    “Update”

--- src/views.py
+++ src/views.py
@@ -207,7 +207,7 @@ def delete_transfer(pos_transfer_id):
 def update_transfer(pos_transfer_id,date_key,book_key,clerk_id,shopper_id,promotion_key,store_key):
     try:
         br = models.getTransfer({"pos_transfer_id": pos_transfer_id})
-        reader = AddReaderForm(request.form, obj=br)
+        reader = AddTransferForm(request.form, obj=br)
         if request.method == 'POST':
             models.updateTransfer({"POS_transfer_id": reader.POS_transfer_id.data, "date_key": reader.date_key.data, "book_key": reader.book_key.data, "clerk_id": reader.clerk_id.data, "shopper_id": reader.shopper_id.data, "promotion_key": reader.promotion_key.data, "store_key": reader.store_key.data})
             return redirect(url_for('transfers'))
